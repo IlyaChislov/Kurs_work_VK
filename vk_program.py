6bc3dfd5cdf08229384f81a0a8da48bcf5618cf8
Ilya_Ch <ilia.spartak@yandex.ru>
2023-12-05 20:38:18 +0300

    -third_try

--- vk_program.py
+++ vk_program.py
@@ -1,70 +1,38 @@
-import requests
+import configparser
 import json
+import logging
+
 import vk_class
 import yandex_class
-# app_id = '51792587'
-# base_url = 'https://oauth.vk.com/authorize'
-# params={
-#     'client_id': app_id,
-#     'redirect_uri': 'https://oauth.vk.com/blank.html',
-#     'display': 'page',
-#     'scope': 'photos,offline',
-#     'response_type': 'token'
-# }
-# response=requests.get(base_url, params=params)
-# print(response.url)
-# app_id='4d319bb29e474d4b894df2b993c6f3cf'
-# base_url='https://oauth.yandex.ru/authorize'
-# params_yandex={
-#     'response_type': 'token',
-#     'client_id': app_id
-# }
-# response=requests.get(base_url, params=params_yandex)
-# print(response.url)
 
-if __name__=='__main__':
-    # owner_id=76879487
-    # token_yandex='y0_AgAAAAAFmdz2AArRrwAAAADx_0S8TuTq6oibSaCtNNXm5Cx94MDHHBs'
-    print("Введите id страницы пользователя вк, owner_id=")
-    owner_id=input()
-    print("Введите токен с полигона яндекс диска, token_yandex=")
-    token_yandex = str(input())
-    vk_page=vk_class.VK_API_PHOTOS(vk_class.token_vk, owner_id)
-    class1=yandex_class.YANDEX_DISK_API(token_yandex, vk_page.get_photos())
-    photos_list=class1.save_photos()
-    with open('data_file,json', "w") as f:
-        json.dump(photos_list, f)
-
-
-
-
-
-# photos=first_page.get_photos()
-# pprint.pprint(photos)
-# list_file_names=[]
-# for files in photos['response']['items']:
-#     file_url=files['sizes'][-1]['url']
-#     file_name=files['likes']['count']
-#     if file_name in list_file_names:
-#         file_name+=str(files['date'])
-#     else:
-#         list_file_names.append(file_name)
-#     photo=requests.get(file_url)
-#     print(file_url)
-#     with open('C:/Users/User/Desktop/save_photos/'+str(file_name)+'jpeg', 'wb') as f:
-#         f.write(photo.content)
-
-
-
-# disk = YaDisk(token=token_yandex)
-# if disk.check_token():
-#     print("Авторизация прошла успешно")
-# else:
-#     print("Ошибка авторизации")
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    parser = configparser.ConfigParser()
+    parser.read('config.ini')
+    token_vk = parser.get('Settings', 'token_vk')
+    token_yandex = parser.get('Settings', 'token_yandex')
+    print('Start programs')
+    print("Введите id или screen_name страницы")
+    screen_name = input()
+    owner_id = vk_class.trans_name_id(token_vk, screen_name)
+    # 76879487
+    # 'y0_AgAAAAAFmdz2AArRrwAAAADx_0S8TuTq6oibSaCtNNXm5Cx94MDHHBs'
+    logging.basicConfig(filename='log_file.log', filemode='w',
+                        encoding='utf-8', level='INFO', format='%(asctime)s, %(levelname)s, %(message)s')
+    logger = logging.getLogger()
+    logger.info('Входные данные о странице пользователя введены')
+    vk_page = vk_class.VK_API_PHOTOS(token_vk, owner_id)
+    length = len(vk_page.get_photos())
+    print(f'Введите количество фото, но не более', length,
+          ', поскольку у пользователя столько фотографий в профиле')
+    count = int(input())
+    if count <= length:
+        logger.info('Пользователь вверно ввёл количество фото')
+        class1 = yandex_class.YANDEX_DISK_API(token_yandex, vk_page.get_photos())
+        photos_list = class1.save_photos(count)
+        logger.info('Фотографии загружены на Яндекс диск')
+        with open('data_file.json', "w") as f:
+            json.dump(photos_list, f)
+        logger.info('Сформирован итоговый json файл')
+    else:
+        print('Пользователь неверно ввёл количество фото')
+        logger.info('Пользователь неверно ввёл количество фото')
